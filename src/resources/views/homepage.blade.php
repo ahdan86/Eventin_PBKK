e858c6e8a94bad655d24eb57e3e6264c1c21f764
Ahdan <ahdanamanullah@gmail.com>
2022-03-30 22:39:22 +0700

    feat: testing for laravel project view

---
+++ src/resources/views/homepage.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mainpage - Siprom ITS</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+        <a class="navbar-brand" href="#">
+            <img src="/images/Logo-ITS.png" height="30" alt="Logo ITS">
+        </a>
+        <button class="btn btn-outline-danger" type="button">Logout</button>
+    </nav>
+</body>
+</html>
